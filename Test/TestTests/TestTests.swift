8269d8d1a7873ca17aadc06536b024ee53571bce
Priyanka Gaikwad <pgaikwad@vocera.com>
2017-02-17 15:34:09 +0530

    Added Unit Test

--- Test/TestTests/TestTests.swift
+++ Test/TestTests/TestTests.swift
@@ -24,6 +24,13 @@ class TestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let val = true
+        if val {
+            XCTAssertTrue(true , ":: 1")
+        } else {
+            XCTAssertTrue(false , ":: 0")
+        }
+        
     }
     
     func testPerformanceExample() {
